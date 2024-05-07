2ea92a041dc82157d9015cd6834b549f58b33280
ReemAlawad <alawadr9@gmail.com>
2024-05-07 14:05:45 +0200

    Add project files.

---
+++ ControllsDemo/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace ControllsDemo.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public string? SelectedOption { get; set; }
+    }
+}
---
+++ ControllsDemo/Models/MyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControllsDemo.Models
+{
+    public class MyViewModel
+    {
+        public int Id { get; set; }
+        public Department? Department { get; set; }
+        public Cat? Cat { get; set; }
+
+        [Required(ErrorMessage ="Selecteer een afdeling")]
+        public int SelectedDepartmentId { get; set; }
+        public string SelectedDepartmentText { get; set; } = string.Empty;
+        public List<SelectListItem>? DepartmentItems  { get; set; } //DropDonled
+
+
+        public int SelectedCatId { get; set; }
+        public string SelectedCatText { get; set; } = string.Empty;
+        public List<SelectListItem>? CatItems { get; set; }
+
+    }
+}
